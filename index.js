037526a784cfdca7ccc56d687a6f509b22f8e24b
Estherbabs <oluwaseyibaba@gmail.com>
2023-01-29 15:14:39 +0000

    we stopped at arrays

---
+++ index.js
@@ -0,0 +1,29 @@
+
+// Let used when a statement might eventually change//
+let a = 2
+a = "Hello World"
+
+// const used when a statement cannot be changed//
+const v = 5
+
+
+// Boolean- used for true or false statement//
+const isStudent = true
+
+// composite types //
+
+//arrays- represents a list of any basic and/or complex types. eg numbers, names//
+
+const products = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+const names = ["Sam", "Eve", "Josh", "Carlos"]
+
+// Elements- A mixture of string, numbers and booleans
+const elements = [2, 3, 4, "Same", 5, "Eve", true]
+
+// an example of Array of Arrays - two dimensional arrays//
+const studentMarks = [["Megan", 2], ["Sade", 3], ["Ade", 4]]
+
+// accessing array elements//
+console.log(studentMarks[0][0])
+console.log(studentMarks[0][0])
+
