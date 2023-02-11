604b82de3ccc4d8f71ef806fffd3b278c9311fd8
Estherbabs <oluwaseyibaba@gmail.com>
2023-02-11 22:59:36 +0000

    Javascript for loop and while loop

--- index.js
+++ index.js
@@ -27,3 +27,38 @@ const studentMarks = [["Megan", 2], ["Sade", 3], ["Ade", 4]]
 console.log(studentMarks[0][0])
 console.log(studentMarks[0][0])
 
+//looping is when you want to perform an action multiple times//
+//for loops//
+//while loops//
+// for of loops//
+//for in loops//
+//higher other functions//
+
+//for loops//
+const n = 5
+for (let i = 1; i <= 0; i++) {
+    
+  console.log("Hello World");  
+}
+
+//while loop//
+let index = 19
+while (index > 0) {
+    console.log(index,"$")  
+    index-=2
+}
+
+
+for (let index = 12; index <= 24; index++) {
+  console.log(index);
+  
+}
+
+const buildings = [1, 44, 12, 22, 89];
+const sum = 0;
+
+for (let index = 0; index < buildings.length; index++) {
+  sum = sum + buildings[index];
+  
+}
+console.log("The sum is: " + sum);
